85e8baa7d8604d528e5bad7276c010349c5928f0
dmkaruev <dmkaruev@mail.ru>
2024-11-20 23:06:10 +0300

    Аргументы для миксинов

---
+++ head.php
@@ -0,0 +1,12 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="//fonts.googleapis.com" />
+    <link rel="preconnect" href="//fonts.gstatic.com" crossorigin />
+    <title></title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <?php
+        echo $css;
+    ?>
+</head>
